e3fd664c60de56f4eb50ca8a5284455f896b3859
vaibhav-sutariya <vsutariya428@gmail.com>
2024-02-17 17:16:46 +0530

    main

--- lib/screens/main/screens/componets/bottomSlideBar.dart
+++ lib/screens/main/screens/componets/bottomSlideBar.dart
@@ -5,6 +5,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:hunger/constants.dart';
 import 'package:hunger/models/UserModal.dart';
 import 'package:hunger/screens/main/screens/SeeAllScreen.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'dart:math' show cos, sqrt, asin;
 import 'package:geolocator/geolocator.dart';
@@ -31,6 +32,7 @@ class BottomSlider extends StatefulWidget {
 class _BottomSliderState extends State<BottomSlider> {
   final PanelController _panelController = PanelController();
 
+  bool _isLoading = true;
   late bool? _showSeeAll = true;
   late double? userLat;
   late double? userLon;
@@ -105,7 +107,9 @@ class _BottomSliderState extends State<BottomSlider> {
             return distanceA.compareTo(distanceB);
           });
 
-          setState(() {});
+          setState(() {
+            _isLoading = false;
+          });
         }
       });
     }
@@ -181,129 +185,161 @@ class _BottomSliderState extends State<BottomSlider> {
           ),
           const SizedBox(height: 10),
           Expanded(
-            child: _userDataList.isEmpty
-                ? const Center(child: Text('No data available'))
-                : ListView.builder(
-                    itemCount: _userDataList.length,
-                    itemBuilder: (context, index) {
-                      var userData = _userDataList[index];
-                      double locationLat =
-                          double.parse(userData.location.split(',')[0]);
-                      double locationLon =
-                          double.parse(userData.location.split(',')[1]);
-                      double distance = userLat != null && userLon != null
-                          ? calculateDistance(
-                              userLat!, userLon!, locationLat, locationLon)
-                          : 0.0;
-                      return Container(
-                        margin: const EdgeInsets.symmetric(vertical: 5.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          border: Border.all(
-                            color: kPrimaryColor,
-                            width: 2.0,
-                          ),
-                        ),
-                        child: ListTile(
-                          contentPadding: const EdgeInsets.all(10.0),
-                          leading: const Icon(
-                            Icons.location_on_outlined,
-                            size: 50,
-                            color: kPrimaryColor,
-                          ),
-                          title: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                userData.fname,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
+            child: _isLoading
+                ? _buildShimmerList()
+                : _userDataList.isEmpty
+                    ? const Center(child: Text('No data available'))
+                    : ListView.builder(
+                        itemCount: _userDataList.length,
+                        itemBuilder: (context, index) {
+                          var userData = _userDataList[index];
+                          double locationLat =
+                              double.parse(userData.location.split(',')[0]);
+                          double locationLon =
+                              double.parse(userData.location.split(',')[1]);
+                          double distance = userLat != null && userLon != null
+                              ? calculateDistance(
+                                  userLat!, userLon!, locationLat, locationLon)
+                              : 0.0;
+                          return Container(
+                            margin: const EdgeInsets.symmetric(vertical: 5.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              border: Border.all(
+                                color: kPrimaryColor,
+                                width: 2.0,
                               ),
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  shadowColor: Colors.white,
-                                  surfaceTintColor: Colors.white,
-                                  visualDensity: const VisualDensity(
-                                    horizontal: -4,
-                                    vertical: -2,
-                                  ),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    side: const BorderSide(
-                                      color: kPrimaryColor,
-                                      width: 1.0,
+                            ),
+                            child: ListTile(
+                              contentPadding: const EdgeInsets.all(10.0),
+                              leading: const Icon(
+                                Icons.location_on_outlined,
+                                size: 50,
+                                color: kPrimaryColor,
+                              ),
+                              title: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    userData.fname,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    _showSeeAll = false;
-                                  });
-                                  _panelController.close();
-                                  print(
-                                      'Directions pressed for ${userData.fname}');
-                                },
-                                child: Text(
-                                  '${distance.toStringAsFixed(2)} km',
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    color: kPrimaryColor,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(userData.address),
-                              const SizedBox(height: 10),
-                              Row(
-                                children: [
-                                  ElevatedButton.icon(
+                                  ElevatedButton(
                                     style: ElevatedButton.styleFrom(
-                                      foregroundColor: Colors.white,
-                                      backgroundColor: kPrimaryColor,
+                                      shadowColor: Colors.white,
+                                      surfaceTintColor: Colors.white,
+                                      visualDensity: const VisualDensity(
+                                        horizontal: -4,
+                                        vertical: -2,
+                                      ),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                        side: const BorderSide(
+                                          color: kPrimaryColor,
+                                          width: 1.0,
+                                        ),
+                                      ),
                                     ),
                                     onPressed: () {
-                                      // setState(() {
-                                      //   _showSeeAll = true;
-                                      // });
+                                      setState(() {
+                                        _showSeeAll = false;
+                                      });
                                       _panelController.close();
                                       print(
                                           'Directions pressed for ${userData.fname}');
-                                      _launchMapsApp(locationLat, locationLon);
                                     },
-                                    icon: const Icon(Icons.directions),
-                                    label: const Text(
-                                      'Directions',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
+                                    child: Text(
+                                      '${distance.toStringAsFixed(2)} km',
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        color: kPrimaryColor,
                                       ),
                                     ),
                                   ),
-                                  const SizedBox(width: 10),
-                                  const Text(
-                                    'View Details',
-                                    style: TextStyle(
-                                      color: kPrimaryColor,
-                                      fontWeight: FontWeight.bold,
-                                      decoration: TextDecoration.underline,
-                                      decorationColor: kPrimaryColor,
-                                    ),
+                                ],
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(userData.address),
+                                  const SizedBox(height: 10),
+                                  Row(
+                                    children: [
+                                      ElevatedButton.icon(
+                                        style: ElevatedButton.styleFrom(
+                                          foregroundColor: Colors.white,
+                                          backgroundColor: kPrimaryColor,
+                                        ),
+                                        onPressed: () {
+                                          // setState(() {
+                                          //   _showSeeAll = true;
+                                          // });
+                                          _panelController.close();
+                                          print(
+                                              'Directions pressed for ${userData.fname}');
+                                          _launchMapsApp(
+                                              locationLat, locationLon);
+                                        },
+                                        icon: const Icon(Icons.directions),
+                                        label: const Text(
+                                          'Directions',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(width: 10),
+                                      const Text(
+                                        'View Details',
+                                        style: TextStyle(
+                                          color: kPrimaryColor,
+                                          fontWeight: FontWeight.bold,
+                                          decoration: TextDecoration.underline,
+                                          decorationColor: kPrimaryColor,
+                                        ),
+                                      )
+                                    ],
                                   )
                                 ],
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildShimmerList() {
+    return ListView.builder(
+      itemCount: 5, // You can adjust the number of shimmering tiles
+      itemBuilder: (context, index) {
+        return Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[100]!,
+          child: ListTile(
+            leading: Container(
+              width: 50.0,
+              height: 50.0,
+              color: Colors.white,
+            ),
+            title: Container(
+              height: 15.0,
+              color: Colors.white,
+            ),
+            subtitle: Container(
+              height: 15.0,
+              color: Colors.white,
+            ),
+          ),
+        );
+      },
     );
   }
 
