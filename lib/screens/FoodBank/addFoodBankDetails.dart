47c9e0bb0e2675627a91159c502b2d9a5ed65f1f
vaibhav-sutariya <vsutariya428@gmail.com>
2024-04-15 18:47:18 +0530

    Add notification services to addFoodBankDetails.dart

--- lib/screens/FoodBank/addFoodBankDetails.dart
+++ lib/screens/FoodBank/addFoodBankDetails.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hunger/components/appBar.dart';
 import 'package:hunger/components/myDrawer.dart';
 import 'package:hunger/screens/FoodBank/componets/FoodBankDetailsForm.dart';
+import 'package:hunger/services/Notification.dart';
 
 class AddFoodBankDetails extends StatefulWidget {
   const AddFoodBankDetails({super.key});
@@ -11,6 +13,26 @@ class AddFoodBankDetails extends StatefulWidget {
 }
 
 class _AddFoodBankDetailsState extends State<AddFoodBankDetails> {
+  NotificationServices notificationServices = NotificationServices();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    notificationServices.requestNotificationPermission();
+    notificationServices.forgroundMessage();
+    notificationServices.firebaseInit(context);
+    notificationServices.setupInteractMessage(context);
+    notificationServices.isTokenRefresh();
+
+    notificationServices.getDeviceToken().then((value) {
+      if (kDebugMode) {
+        print('device token');
+        print(value);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
