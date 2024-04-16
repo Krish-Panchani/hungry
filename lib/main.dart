90033b49194d9cc2c97ca4ee477452df74beb489
vaibhav-sutariya <vsutariya428@gmail.com>
2024-04-16 14:21:19 +0530

    Refactor bottomSlideBar.dart, ViewDetails.dart, and main.dart components

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:hunger/Provider/UserDataProvider.dart';
 import 'package:hunger/constants.dart';
 import 'package:hunger/firebase_options.dart';
 import 'package:hunger/screens/splashScreen.dart';
-import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => UserDataProvider(),
-      child: MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/main/screens/componets/ViewDetails.dart
+++ lib/screens/main/screens/componets/ViewDetails.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hunger/Provider/UserDataProvider.dart';
 import 'package:hunger/components/appBar.dart';
 import 'package:hunger/components/myDrawer.dart';
-import 'package:provider/provider.dart';
 
 class ViewDetailsScreen extends StatefulWidget {
   const ViewDetailsScreen({super.key});
@@ -14,22 +12,13 @@ class ViewDetailsScreen extends StatefulWidget {
 class _ViewDetailsScreenState extends State<ViewDetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    final userDataProvider = Provider.of<UserDataProvider>(context);
-    final userData = userDataProvider.selectedUserData!;
     return Scaffold(
       appBar: MyAppBar(),
       drawer: const MyDrawer(
         showLogOut: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Name: ${userData.fname}'),
-            Text('Address: ${userData.address}'),
-            // Add other details as needed
-          ],
-        ),
+      body: const Center(
+        child: Text('View Details'),
       ),
     );
   }
