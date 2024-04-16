aa67ba42349084d5cc346a6c7b8e96a1eee1ba3b
vaibhav-sutariya <vsutariya428@gmail.com>
2024-04-16 12:18:00 +0530

    Update itemCount in _buildShimmerList to use fixed value of 10

--- lib/screens/main/screens/componets/bottomSlideBar.dart
+++ lib/screens/main/screens/componets/bottomSlideBar.dart
@@ -334,8 +334,7 @@ class _BottomSliderState extends State<BottomSlider> {
 
   Widget _buildShimmerList() {
     return ListView.builder(
-      itemCount:
-          _userDataList.length, // You can adjust the number of shimmering tiles
+      itemCount: 10, // You can adjust the number of shimmering tiles
       itemBuilder: (context, index) {
         return Shimmer.fromColors(
           baseColor: Colors.grey[300]!,
