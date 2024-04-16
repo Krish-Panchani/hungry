62d74cf704e06a26ca7cf465ea09a7111ff72bef
vaibhav-sutariya <vsutariya428@gmail.com>
2024-04-16 13:55:53 +0530

    Refactor ViewDetailsScreen component to display user data

--- lib/screens/main/screens/componets/ViewDetails.dart
+++ lib/screens/main/screens/componets/ViewDetails.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hunger/Provider/UserDataProvider.dart';
 import 'package:hunger/components/appBar.dart';
 import 'package:hunger/components/myDrawer.dart';
+import 'package:provider/provider.dart';
 
 class ViewDetailsScreen extends StatefulWidget {
   const ViewDetailsScreen({super.key});
@@ -12,13 +14,22 @@ class ViewDetailsScreen extends StatefulWidget {
 class _ViewDetailsScreenState extends State<ViewDetailsScreen> {
   @override
   Widget build(BuildContext context) {
+    final userDataProvider = Provider.of<UserDataProvider>(context);
+    final userData = userDataProvider.selectedUserData!;
     return Scaffold(
       appBar: MyAppBar(),
       drawer: const MyDrawer(
         showLogOut: true,
       ),
-      body: const Center(
-        child: Text('View Details'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Name: ${userData.fname}'),
+            Text('Address: ${userData.address}'),
+            // Add other details as needed
+          ],
+        ),
       ),
     );
   }
