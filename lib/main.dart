8211c812b1af2e33d68bccccf970cba8e3cb2ae0
vaibhav-sutariya <vsutariya428@gmail.com>
2024-04-16 13:55:35 +0530

    adding notifylisteners to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hunger/Provider/UserDataProvider.dart';
 import 'package:hunger/constants.dart';
 import 'package:hunger/firebase_options.dart';
 import 'package:hunger/screens/splashScreen.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => UserDataProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
