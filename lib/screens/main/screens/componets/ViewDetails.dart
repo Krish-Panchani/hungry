20da099b79143b26cfe68a58c8625adb5c5c84b6
vaibhav-sutariya <vsutariya428@gmail.com>
2024-04-18 19:34:10 +0530

    Refactor ViewDetailsScreen component to display user data and update UI

--- lib/screens/main/screens/componets/ViewDetails.dart
+++ lib/screens/main/screens/componets/ViewDetails.dart
@@ -16,95 +16,150 @@ class _ViewDetailsScreenState extends State<ViewDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: MyAppBar(),
-        drawer: const MyDrawer(
-          showLogOut: true,
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(20),
-          child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(
+      appBar: MyAppBar(),
+      drawer: const MyDrawer(
+        showLogOut: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: kPrimaryColor,
+              width: 2,
+            ),
+          ),
+          child: Column(
+            children: [
+              Container(
+                height: 90,
+                decoration: BoxDecoration(
                   color: kPrimaryColor,
-                  width: 2,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                  border: Border.all(
+                    color: kPrimaryColor,
+                    width: 2,
+                  ),
                 ),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    height: 90,
-                    decoration: BoxDecoration(
-                      color: kPrimaryColor,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20),
-                      ),
-                      border: Border.all(
-                        color: kPrimaryColor,
-                        width: 2,
-                      ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.location_on_outlined,
+                      size: 50,
+                      color: Colors.white,
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Icon(
-                          Icons.location_on_outlined,
-                          size: 50,
-                          color: Colors.white,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              widget.userData.fname,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              widget.userData.address,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shadowColor: Colors.white,
-                            surfaceTintColor: Colors.white,
-                            visualDensity: const VisualDensity(
-                              horizontal: -4,
-                              vertical: -2,
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              side: const BorderSide(
-                                color: kPrimaryColor,
-                                width: 1.0,
-                              ),
-                            ),
+                        Text(
+                          widget.userData.fname,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                           ),
-                          onPressed: () {},
-                          child: Text(
-                            '1.0 km',
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: kPrimaryColor,
-                            ),
+                        ),
+                        Text(
+                          widget.userData.address,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              )),
-        ));
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shadowColor: Colors.white,
+                        surfaceTintColor: Colors.white,
+                        visualDensity: const VisualDensity(
+                          horizontal: -4,
+                          vertical: -2,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          side: const BorderSide(
+                            color: kPrimaryColor,
+                            width: 1.0,
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        '1.0 km',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      widget.userData.details,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Divider(
+                            color: kPrimaryColor,
+                            thickness: 2,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Basic Details",
+                            style: TextStyle(
+                              color: kPrimaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: kPrimaryColor,
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
