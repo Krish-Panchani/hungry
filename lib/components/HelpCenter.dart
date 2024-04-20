2517b2ceef6594e8e27dac55f0dddc5792d3a921
vaibhav-sutariya <vsutariya428@gmail.com>
2024-04-20 08:56:10 +0530

    Add HelpCenterScreen component

---
+++ lib/components/HelpCenter.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HelpCenterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Help Center'),
+      ),
+      body: Center(
+        child: Text('Help Center Screen'),
+      ),
+    );
+  }
+}
